7745e96b48850b9fa5ff7e064374363fe41c338a
Cameron Pfiffer <cpfiffer@gmail.com>
2018-03-19 19:26:51 -0700

    Init

--- src/Orderbook.jl
+++ src/Orderbook.jl
@@ -1,5 +1,19 @@
 module Orderbook
-
 # package code goes here
+
+mutable struct Book
+   orders::
+end
+
+gdax_l3 = """{
+    "type": "received",
+    "time": "2014-11-07T08:19:27.028459Z",
+    "product_id": "BTC-USD",
+    "sequence": 10,
+    "order_id": "d50ec984-77a8-460a-b958-66f114b0de9b",
+    "size": "1.34",
+    "price": "502.1",
+    "side": "buy",
+    "order_type": "limit"}""";
 
 end # module
